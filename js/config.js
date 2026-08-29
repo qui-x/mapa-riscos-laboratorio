b29207d30b2ece1e08efae98e2a9c1e206fd3b80
qui-x <thiagoweslei11@gmail.com>
2026-08-29 10:27:13 -0300

    atualizar

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 /** Configuração do frontend para integração com o Google Apps Script. */
 window.APP_CONFIG = Object.freeze({
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbxIXZyqsm40slkz1cX-Q8o-BhTb2OTMs5-iWjxXJeBjTQrgoqfJnRpDmeq8ZlQz7Imlbw/exec'
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbyKbrsHTlyrmuw6zDS7w-kPYgM7vD3yHbckiJYP9s44PmFAw18TiOSO7g0HcLaq8TUZ6A/exec'
 });
